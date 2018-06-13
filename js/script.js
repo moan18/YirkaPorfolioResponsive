9c2dc90e0f37abf7c87420d5544098dc81a472d0
moan <moan18@gmail.com>
2018-06-13 12:34:07 -0500

    Home, About, Gallery Rinconada

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(window).load(function(){
+
+$("#f1").hover(function() {
+  $('body').removeClass();
+  $('body').addClass('class1');
+});
+
+$("#f2").hover(function() {
+  $('body').removeClass();
+  $('body').addClass('class2');
+});
+
+$("#f3").hover(function() {
+  $('body').removeClass();
+  $('body').addClass('class3');
+});
+
+$("#f4").hover(function() {
+  $('body').removeClass();
+  $('body').addClass('class4');
+});
+
+$("#f5").hover(function() {
+  $('body').removeClass();
+  $('body').addClass('class5');
+});
+
+$("#f6").hover(function() {
+  $('body').removeClass();
+  $('body').addClass('class6');
+});
+
+
+});//]]> 
